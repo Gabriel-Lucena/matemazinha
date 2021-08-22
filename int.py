d7147d4ac05c9f70d9cb29eca34437028cc885a9
Gabriel-Lucena <gabriel.lucena.cunha@hotmail.com>
2021-08-22 02:11:07 -0300

    Avanço na interface

--- int.py
+++ int.py
@@ -4,12 +4,23 @@ from tkinter import *
 from bib import *
 
 janela = Tk()
-janela.title("Triangulos")
+janela.title("Ματεμα")
 
-texto_orientacao = Label(janela, text = "Clique no botão para ver a cotação das moedas" )
-texto_orientacao.grid(column=0,row=0)
+janela.geometry('1000x1000+50+50')
+janela.resizable(True, True)
+janela.iconbitmap('img/favicon.ico')
 
-butao = Button(janela, text="Calcular", command=triangulo_sem_angulo)
-butao.grid(column=0,row=1)
+texto_orientacao = Label(janela, text = "Raízes de uma equação do segundo grau:", font=("Helvetica", 16))
+texto_orientacao.place(x=475,y=250)
+
+label_1 = Label(janela, text='Digite aqui os coeficientes:', font=("Helvetica", 14))
+label_1.place(relx=0, rely=0.2, relwidth=0.5, anchor='ne')
+
+campo_de_texto_a = Text(janela, height=8, font=("Helvetica", 10))
+campo_de_texto_a.place(x=475,y=300)
+
+
+butao = Button(janela, text="Calcular", command=triangulo_sem_angulo, font=("Helvetica", 10))
+
 
 janela.mainloop()
