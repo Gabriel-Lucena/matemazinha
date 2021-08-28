72fab451f821144c0e07f370f8c39217ef1bbc33
Gabriel-Lucena <gabriel.lucena.cunha@hotmail.com>
2021-08-27 21:45:05 -0300

    Método: Área sob a função no intervalo das raízes

--- bib.py
+++ bib.py
@@ -2,7 +2,8 @@ import math
 from decimal import Decimal
 from tkinter import StringVar
 
-def classificar_triangulo_pelos_lados(lado_a,lado_b,lado_c):
+
+def classificar_triangulo_pelos_lados(lado_a, lado_b, lado_c):
 
     if lado_a == lado_b or lado_b == lado_c or lado_a == lado_c:
 
@@ -22,6 +23,7 @@ def classificar_triangulo_pelos_lados(lado_a,lado_b,lado_c):
         print('O triângulo é escaleno.')
         print('--------------------------------------')
 
+
 def verificar_soma(a, b):
     c = a + b
 
@@ -34,9 +36,10 @@ def verificar_soma(a, b):
 
     print('--------------')
 
+
 def verificar_se_e_inteiro(numero_real):
 
-    if isinstance(numero_real,int):
+    if isinstance(numero_real, int):
 
         return True
 
@@ -44,10 +47,11 @@ def verificar_se_e_inteiro(numero_real):
 
         return False
 
-def verificar_se_triangulo_existe_com_angulos(lado_a,lado_b,lado_c,angulo_a,angulo_b,angulo_c):
-    
+
+def verificar_se_triangulo_existe_com_angulos(lado_a, lado_b, lado_c, angulo_a, angulo_b, angulo_c):
+
     if math.fabs(lado_b - lado_c) < lado_a < lado_b + lado_c and math.fabs(lado_a - lado_c) < lado_b < lado_a + lado_c and math.fabs(lado_a - lado_b) < lado_c < lado_b + lado_a and angulo_a + angulo_b + angulo_c == 180:
-      
+
         print('--------------------------------------')
         print('O triângulo existe.')
         print('--------------------------------------')
@@ -62,7 +66,8 @@ def verificar_se_triangulo_existe_com_angulos(lado_a,lado_b,lado_c,angulo_a,angu
 
         return False
 
-def verificar_se_triangulo_existe_simples(lado_a,lado_b,lado_c):
+
+def verificar_se_triangulo_existe_simples(lado_a, lado_b, lado_c):
 
     if math.fabs(lado_b - lado_c) < lado_a < lado_b + lado_c and math.fabs(lado_a - lado_c) < lado_b < lado_a + lado_c and math.fabs(lado_a - lado_b) < lado_c < lado_b + lado_a:
 
@@ -70,7 +75,7 @@ def verificar_se_triangulo_existe_simples(lado_a,lado_b,lado_c):
         print('O triângulo existe.')
         print('--------------------------------------')
 
-        return True 
+        return True
 
     else:
 
@@ -80,7 +85,8 @@ def verificar_se_triangulo_existe_simples(lado_a,lado_b,lado_c):
 
         return False
 
-def verificar_se_triangulo_e_retangulo_com_pitagoras(lado_a,lado_b,lado_c):
+
+def verificar_se_triangulo_e_retangulo_com_pitagoras(lado_a, lado_b, lado_c):
 
     if lado_a ** 2 + lado_b ** 2 == lado_c ** 2 or lado_b ** 2 + lado_c ** 2 == lado_a ** 2 or lado_a ** 2 + lado_c ** 2 == lado_b ** 2:
 
@@ -98,15 +104,16 @@ def verificar_se_triangulo_e_retangulo_com_pitagoras(lado_a,lado_b,lado_c):
 
         return False
 
-def verificar_se_triangulo_e_retangulo(lado_a,lado_b,lado_c,angulo_a,angulo_b,angulo_c):
+
+def verificar_se_triangulo_e_retangulo(lado_a, lado_b, lado_c, angulo_a, angulo_b, angulo_c):
 
     if (lado_a ** 2 + lado_b ** 2 == lado_c ** 2 or lado_b ** 2 + lado_c ** 2 == lado_a ** 2 or lado_a ** 2 + lado_c ** 2 == lado_b ** 2) and (angulo_a == 90 or angulo_b == 90 or angulo_c == 90):
-          
+
         print('--------------------------------------')
         print('O triângulo é retângulo.')
         print('--------------------------------------')
 
-        classificar_triangulo_pelos_lados(lado_a,lado_b,lado_c)
+        classificar_triangulo_pelos_lados(lado_a, lado_b, lado_c)
 
         return True
 
@@ -114,7 +121,8 @@ def verificar_se_triangulo_e_retangulo(lado_a,lado_b,lado_c,angulo_a,angulo_b,an
 
         return False
 
-def verificar_se_triangulo_e_obtusangulo(lado_a,lado_b,lado_c,angulo_a,angulo_b,angulo_c):
+
+def verificar_se_triangulo_e_obtusangulo(lado_a, lado_b, lado_c, angulo_a, angulo_b, angulo_c):
 
     if (lado_a ** 2 + lado_b ** 2 > lado_c ** 2 or lado_c ** 2 + lado_b ** 2 > lado_a ** 2 or lado_a ** 2 + lado_c ** 2 > lado_b ** 2) and (angulo_a > 90 or angulo_b > 90 or angulo_c > 90):
 
@@ -122,7 +130,7 @@ def verificar_se_triangulo_e_obtusangulo(lado_a,lado_b,lado_c,angulo_a,angulo_b,
         print('O triângulo é obtusângulo.')
         print('--------------------------------------')
 
-        classificar_triangulo_pelos_lados(lado_a,lado_b,lado_c)
+        classificar_triangulo_pelos_lados(lado_a, lado_b, lado_c)
 
         return True
 
@@ -130,7 +138,8 @@ def verificar_se_triangulo_e_obtusangulo(lado_a,lado_b,lado_c,angulo_a,angulo_b,
 
         return False
 
-def verificar_se_triangulo_e_acutangulo(lado_a,lado_b,lado_c,angulo_a,angulo_b,angulo_c):
+
+def verificar_se_triangulo_e_acutangulo(lado_a, lado_b, lado_c, angulo_a, angulo_b, angulo_c):
 
     if (lado_a**2 + lado_b**2 < lado_c**2 or lado_a**2 + lado_c**2 < lado_b**2 or lado_c**2 + lado_b**2 < lado_a**2) and (angulo_a < 90 or angulo_b < 90 or angulo_c < 90):
 
@@ -138,13 +147,14 @@ def verificar_se_triangulo_e_acutangulo(lado_a,lado_b,lado_c,angulo_a,angulo_b,a
         print('O triângulo é acutângulo.')
         print('--------------------------------------')
 
-        classificar_triangulo_pelos_lados(lado_a,lado_b,lado_c)
+        classificar_triangulo_pelos_lados(lado_a, lado_b, lado_c)
 
         return True
 
     else:
 
         return False
+
 
 def interface_terminal_com_segundo_grau():
 
@@ -153,25 +163,28 @@ def interface_terminal_com_segundo_grau():
     b = int(input('b: '))
     c = int(input('c: '))
 
-    segundo_grau_melhorada(a,b,c)
+    segundo_grau_melhorada(a, b, c)
+
 
 def simplificar_raiz(raiz):
 
-    if isinstance(raiz ** 0.5,int):
+    if isinstance(raiz ** 0.5, int):
 
         raiz = raiz ** 0.5
 
         return round(raiz)
-    
+
     else:
 
         return raiz
 
-def calcular_delta(a,b,c):
+
+def calcular_delta(a, b, c):
 
     delta = b ** 2 - 4 * a * c
 
     return delta
+
 
 def verificar_idade(idade):
 
@@ -186,39 +199,44 @@ def verificar_idade(idade):
 
     print('----------------------')
 
-def segundo_grau_calcular_raiz_1(a,b,c):
 
-    raiz_1 = (-b - math.sqrt(calcular_delta(a,b,c))) / (2 * a)
+def segundo_grau_calcular_raiz_1(a, b, c):
+
+    raiz_1 = (-b - math.sqrt(calcular_delta(a, b, c))) / (2 * a)
 
     return raiz_1
 
-def segundo_grau_calcular_raiz_2(a,b,c):
 
-    raiz_2 = (-b + math.sqrt(calcular_delta(a,b,c))) / (2 * a)
+def segundo_grau_calcular_raiz_2(a, b, c):
+
+    raiz_2 = (-b + math.sqrt(calcular_delta(a, b, c))) / (2 * a)
 
     return raiz_2
 
-def segundo_grau_convertido_raiz_1(a,b,c):
 
-    x1_convertido = Decimal(segundo_grau_calcular_raiz_1(a,b,c))
+def segundo_grau_convertido_raiz_1(a, b, c):
+
+    x1_convertido = Decimal(segundo_grau_calcular_raiz_1(a, b, c))
 
     return x1_convertido
 
-def segundo_grau_convertido_raiz_2(a,b,c):
 
-    x2_convertido = Decimal(segundo_grau_calcular_raiz_2(a,b,c))
+def segundo_grau_convertido_raiz_2(a, b, c):
+
+    x2_convertido = Decimal(segundo_grau_calcular_raiz_2(a, b, c))
 
     return x2_convertido
 
-def segundo_grau_comparar_raizes(a,b,c):
 
-    if segundo_grau_calcular_raiz_1(a,b,c) == segundo_grau_convertido_raiz_1(a,b,c) and segundo_grau_calcular_raiz_2(a,b,c) == segundo_grau_convertido_raiz_2(a,b,c):
+def segundo_grau_comparar_raizes(a, b, c):
+
+    if segundo_grau_calcular_raiz_1(a, b, c) == segundo_grau_convertido_raiz_1(a, b, c) and segundo_grau_calcular_raiz_2(a, b, c) == segundo_grau_convertido_raiz_2(a, b, c):
 
         print('Há duas raízes diferentes:')
         print('x_1')
-        print(segundo_grau_convertido_raiz_1(a,b,c))
+        print(segundo_grau_convertido_raiz_1(a, b, c))
         print('x_2')
-        print(segundo_grau_convertido_raiz_2(a,b,c))
+        print(segundo_grau_convertido_raiz_2(a, b, c))
 
         return True
 
@@ -226,22 +244,22 @@ def segundo_grau_comparar_raizes(a,b,c):
 
         print('Há duas raízes diferentes:')
         print('x_1')
-        print(segundo_grau_calcular_raiz_1(a,b,c))
+        print(segundo_grau_calcular_raiz_1(a, b, c))
         print('x_2')
-        print(segundo_grau_calcular_raiz_2(a,b,c))
+        print(segundo_grau_calcular_raiz_2(a, b, c))
 
         return False
 
-def segundo_grau_calcular_raizes_complexas(a,b,c):
 
-    delta_negativo = -calcular_delta(a,b,c)
+def segundo_grau_calcular_raizes_complexas(a, b, c):
+
+    delta_negativo = -calcular_delta(a, b, c)
 
     raiz_1 = (-b) / (2 * a)
     raiz_complexa_1 = (- math.sqrt(delta_negativo)) / (2 * a)
     raiz_2 = (-b) / (2 * a)
     raiz_complexa_2 = (+ math.sqrt(delta_negativo)) / (2 * a)
 
-    
     print('Há duas raízes complexas:')
     print('x_1')
     print(' Parte real:', raiz_1)
@@ -252,7 +270,8 @@ def segundo_grau_calcular_raizes_complexas(a,b,c):
 
     return delta_negativo
 
-def segundo_grau_calcular_raiz(a,b):
+
+def segundo_grau_calcular_raiz(a, b):
 
     # Versão simplificada em que se mostra somente o resultado em decimais
 
@@ -271,34 +290,36 @@ def segundo_grau_calcular_raiz(a,b):
         print('x')
         print(raiz)
 
-def segundo_grau(a,b,c):
+
+def segundo_grau(a, b, c):
 
     # Delta maior que 0
 
-    if calcular_delta(a,b,c) > 0:
+    if calcular_delta(a, b, c) > 0:
 
-        segundo_grau_comparar_raizes(a,b,c)
+        segundo_grau_comparar_raizes(a, b, c)
 
     # Delta menor que 0
 
-    elif calcular_delta(a,b,c) < 0:
+    elif calcular_delta(a, b, c) < 0:
 
-        segundo_grau_calcular_raizes_complexas(a,b,c)
+        segundo_grau_calcular_raizes_complexas(a, b, c)
 
     # Delta igual a 0
 
     else:
 
-        segundo_grau_calcular_raiz(a,b)
+        segundo_grau_calcular_raiz(a, b)
 
     print('----------------------')
 
 # Versão melhorada
 # Aparece a resposta como se fosse feito à mão
 
+
 def segundo_grau_melhorada(a, b, c):
 
-    if calcular_delta(a,b,c) == 0:
+    if calcular_delta(a, b, c) == 0:
 
         print('Há duas raízes iguais reais ou uma raiz real:')
         print('x_1:')
@@ -310,29 +331,29 @@ def segundo_grau_melhorada(a, b, c):
         print('----')
         print(' ', 2 * a, ' ')
 
-    elif calcular_delta(a,b,c) > 0:
+    elif calcular_delta(a, b, c) > 0:
 
-        if verificar_se_e_inteiro(simplificar_raiz(calcular_delta(a,b,c))) == True:
+        if verificar_se_e_inteiro(simplificar_raiz(calcular_delta(a, b, c))) == True:
 
             print('Há duas raízes diferentes e reais:')
             print('x_1:')
-            print(-1 * b, ' - ', calcular_delta(a,b,c) ** 0.5)
+            print(-1 * b, ' - ', calcular_delta(a, b, c) ** 0.5)
             print('--------------------------')
             print('         ', 2 * a, '        ')
             print('x_2:')
-            print(-1 * b, ' + ', calcular_delta(a,b,c) ** 0.5)
+            print(-1 * b, ' + ', calcular_delta(a, b, c) ** 0.5)
             print('--------------------------')
-            print('         ', 2 * a, '        ')       
+            print('         ', 2 * a, '        ')
 
         else:
 
             print('Há duas raízes diferentes e reais:')
             print('x_1:')
-            print(-1 * b, ' - sqrt(', calcular_delta(a,b,c), ')')
+            print(-1 * b, ' - sqrt(', calcular_delta(a, b, c), ')')
             print('--------------------------')
             print('         ', 2 * a, '        ')
             print('x_2:')
-            print(-1 * b, ' + sqrt(', calcular_delta(a,b,c), ')')
+            print(-1 * b, ' + sqrt(', calcular_delta(a, b, c), ')')
             print('--------------------------')
             print('         ', 2 * a, '        ')
 
@@ -340,33 +361,35 @@ def segundo_grau_melhorada(a, b, c):
 
         print('Há duas raízes complexas conjugadas:')
         print('x_1:')
-        print(-1 * b, ' - sqrt(', abs(calcular_delta(a,b,c)), ') i')
+        print(-1 * b, ' - sqrt(', abs(calcular_delta(a, b, c)), ') i')
         print('--------------------------')
         print('         ', 2 * a, '        ')
         print('x_2:')
-        print(-1 * b, ' + sqrt(', abs(calcular_delta(a,b,c)), ') i')
+        print(-1 * b, ' + sqrt(', abs(calcular_delta(a, b, c)), ') i')
         print('--------------------------')
         print('         ', 2 * a, '        ')
 
     print('----------------------')
 
+
 def triangulo(lado_a, lado_b, lado_c, angulo_a, angulo_b, angulo_c):
 
-  if verificar_se_triangulo_existe_com_angulos(lado_a, lado_b, lado_c, angulo_a, angulo_b, angulo_c):
+    if verificar_se_triangulo_existe_com_angulos(lado_a, lado_b, lado_c, angulo_a, angulo_b, angulo_c):
 
-    if verificar_se_triangulo_e_retangulo(lado_a,lado_b,lado_c,angulo_a,angulo_b,angulo_c):
+        if verificar_se_triangulo_e_retangulo(lado_a, lado_b, lado_c, angulo_a, angulo_b, angulo_c):
 
-        return
+            return
 
-    elif verificar_se_triangulo_e_obtusangulo(lado_a,lado_b,lado_c,angulo_a,angulo_b,angulo_c):
+        elif verificar_se_triangulo_e_obtusangulo(lado_a, lado_b, lado_c, angulo_a, angulo_b, angulo_c):
 
-        return
+            return
 
-    elif verificar_se_triangulo_e_acutangulo(lado_a,lado_b,lado_c,angulo_a,angulo_b,angulo_c):
+        elif verificar_se_triangulo_e_acutangulo(lado_a, lado_b, lado_c, angulo_a, angulo_b, angulo_c):
 
-        return
+            return
 
-def exibir_maximo_ou_minimo(a,b):
+
+def exibir_maximo_ou_minimo(a, b):
 
     if a == 0:
 
@@ -376,31 +399,20 @@ def exibir_maximo_ou_minimo(a,b):
 
     elif a > 0:
 
-        print('--------------------------------------') 
+        print('--------------------------------------')
         print('A abscissa do ponto mínimo é:')
-        print(determinar_abscissa_do_ponto_maximo_ou_minimo(a,b))
+        print(determinar_abscissa_do_ponto_maximo_ou_minimo(a, b))
         print('--------------------------------------')
 
     else:
 
-        print('--------------------------------------') 
+        print('--------------------------------------')
         print('A abscissa do ponto máximo é:')
-        print(determinar_abscissa_do_ponto_maximo_ou_minimo(a,b))
+        print(determinar_abscissa_do_ponto_maximo_ou_minimo(a, b))
         print('--------------------------------------')
 
-def determinar_abscissa_do_ponto_maximo_ou_minimo(a,b):
 
-        if a == 0:
-
-            return False
-
-        else:
-
-            abscissa_maximo_ou_minimo = - b * ( 2 * a ) ** -1
-
-            return abscissa_maximo_ou_minimo
-
-def determinar_ordenada_do_ponto_maximo_ou_minimo(a,b,c):
+def determinar_abscissa_do_ponto_maximo_ou_minimo(a, b):
 
     if a == 0:
 
@@ -408,9 +420,23 @@ def determinar_ordenada_do_ponto_maximo_ou_minimo(a,b,c):
 
     else:
 
-        ordenada_do_ponto_maximo = -1 * calcular_delta(a,b,c) * ( 4 * a ) ** -1
+        abscissa_maximo_ou_minimo = - b * (2 * a) ** -1
+
+        return abscissa_maximo_ou_minimo
+
+
+def determinar_ordenada_do_ponto_maximo_ou_minimo(a, b, c):
+
+    if a == 0:
+
+        return False
+
+    else:
+
+        ordenada_do_ponto_maximo = -1 * calcular_delta(a, b, c) * (4 * a) ** -1
 
         return ordenada_do_ponto_maximo
+
 
 def teste_de_concavidade(a):
 
@@ -426,17 +452,42 @@ def teste_de_concavidade(a):
 
         return 'A função tem a concavidade voltada para baixo.'
 
+
 def triangulo_sem_angulo(lado_a, lado_b, lado_c):
 
-    if verificar_se_triangulo_existe_simples(lado_a,lado_b,lado_c):
+    if verificar_se_triangulo_existe_simples(lado_a, lado_b, lado_c):
 
-        if verificar_se_triangulo_e_retangulo_com_pitagoras(lado_a,lado_b,lado_c):
+        if verificar_se_triangulo_e_retangulo_com_pitagoras(lado_a, lado_b, lado_c):
 
-            classificar_triangulo_pelos_lados(lado_a,lado_b,lado_c)
+            classificar_triangulo_pelos_lados(lado_a, lado_b, lado_c)
 
         else:
 
             return
+
+
+def area_sob_uma_equacao_do_segundo_grau_num_intervalo_fechado(a, b, c, x_1, x_2):
+
+    if a == 0:
+
+        return False
+
+    else:
+
+        area = (a * x_2 ** 3- a * x_1 ** 3) /3 + (b * x_2 ** 2 - b * x_1 ** 2) / 2 + c * x_2 - c * x_1
+
+        return area
+
+
+def area_sob_equacao_do_segundo_grau(a,b,c):
+
+    area = area_sob_uma_equacao_do_segundo_grau_num_intervalo_fechado(a, b, c, segundo_grau_calcular_raiz_1(a,b,c), segundo_grau_calcular_raiz_2(a,b,c))
+
+    print('--------------------------------------')
+    print('A área sob a função é:')
+    print('--------------------------------------')
+    print(area)
+    print('--------------------------------------')
 
 class FuncaoSegundoGrau:
 
@@ -462,7 +513,11 @@ class FuncaoSegundoGrau:
 
         print('--------------------------------------')
         print('A abscissa:')
-        print(determinar_abscissa_do_ponto_maximo_ou_minimo(self.a,self.b))
+        print(determinar_abscissa_do_ponto_maximo_ou_minimo(self.a, self.b))
         print('A ordenada:')
-        print(determinar_ordenada_do_ponto_maximo_ou_minimo(self.a,self.b,self.c))
+        print(determinar_ordenada_do_ponto_maximo_ou_minimo(self.a, self.b, self.c))
         print('--------------------------------------')
+
+    #def area_sob_a_curva_entre_as_raizes(self):
+
+
